13d3f270b2f30a72f149b7d98e40784775f831f0
chomper <1538477318@qq.com>
2018-07-01 12:12:54 +0800

    7.1-12.12

--- app01/views.py
+++ app01/views.py
@@ -9,10 +9,14 @@ def news(request,nid1,nid2):
     return HttpResponse(nid1+nid2)
 
 def db_handle(request):
-    #models.userinfo.objects.create(name='recoba',age=40)
+    #models.userinfo.objects.create(name='int',age=55)
     #models.userinfo.objects.filter(name='jojo').delete()
     #models.userinfo.objects.filter(name='jojo').update(age=30)
     user_list_obj = models.userinfo.objects.all()
-    #for line in user_list_obj:
-    #    print line.name
+    models.userinfo.objects.create(
+        name=request.POST[u'name'],
+        age=request.POST[u'age'],
+    )
+    #print request.POST[u'name']
+    #print type(request.POST[u'name'])
     return render(request,'t1.html',{'li':user_list_obj})
