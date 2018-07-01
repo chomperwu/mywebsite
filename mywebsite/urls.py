c6c11c25a7bf0714f613d91be2246398adf0f58a
chomper <1538477318@qq.com>
2018-07-01 23:01:09 +0800

    7.1-23.00

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -13,14 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
-from app01 import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^home/', views.home),
-    url(r'^news/(\d+)/(\d+)', views.news),
-    url(r'^db_handle/', views.db_handle),
+    url(r'^payment/',include("app01.urls"))
 ]
 
