7ebd4f46072291e5b0ca3cb60db6efb4067fd6db
chomper <1538477318@qq.com>
2018-07-02 17:36:46 +0800

    2018.7.2-17:36

--- app01/urls.py
+++ app01/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url,include
 from app01 import views
 
 urlpatterns = [
-    url(r'^home/', views.home),
+    url(r'^home/', views.home, {"userman":"wd"}),
     url(r'^news/(\d+)/(\d+)', views.news),
     url(r'^db_handle/', views.db_handle),
 ]
--- app01/views.py
+++ app01/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render,HttpResponse
 from app01 import models
 # Create your views here.
 
-def home(request):
-    return HttpResponse('OK')
+def home(request,userman):
+    return HttpResponse("OK,it' %s ." %userman)
 
 def news(request,nid1,nid2):
     return HttpResponse(nid1+nid2)
