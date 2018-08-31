e5c4c50891b6a70fe94e29d360cb8c99d00badaa
chomperwu <1538477318@qq.com>
2018-08-31 14:37:09 +0800

    2018.8.31-14:36

--- app01/views.py
+++ app01/views.py
@@ -9,14 +9,18 @@ def news(request,nid1,nid2):
     return HttpResponse(nid1+nid2)
 
 def db_handle(request):
-    #models.userinfo.objects.create(name='int',age=55)
     #models.userinfo.objects.filter(name='jojo').delete()
     #models.userinfo.objects.filter(name='jojo').update(age=30)
+
+    #models.userinfo.objects.create(name=request.POST['username'],
+    #                               age=request.POST['userage']
+    #                               )
+    if request.POST.get('username') and request.POST.get('userage'):
+        models.userinfo.objects.create(name=request.POST.get('username'),
+                                       age=request.POST.get('userage')
+                                      )
     user_list_obj = models.userinfo.objects.all()
-    models.userinfo.objects.create(
-        name=request.POST[u'name'],
-        age=request.POST[u'age'],
-    )
-    #print request.POST[u'name']
-    #print type(request.POST[u'name'])
+    print request.POST.get('userage')
+    print request.POST
+    #print type(request.POST)
     return render(request,'t1.html',{'li':user_list_obj})
