2d28de92ea62aa51c3e65bfaea005dc8155aae70
Administrator <1538477318@qq.com>
2019-01-04 16:39:44 +0800

    20190104-16:39

--- app01/urls.py
+++ app01/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url,include
 from app01 import views
 
 urlpatterns = [
-    url(r'^home/', views.home, {"userman":"wd"}),
+    url(r'^home/', views.home, {"userman":"wd11"}),
     url(r'^news/(\d+)/(\d+)', views.news),
     url(r'^db_handle/', views.db_handle),
 ]
